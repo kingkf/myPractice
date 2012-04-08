ebcaa269ebc1a282f5bde5b727990713c6598b74
Kaifeng Jin <jkfbupt@gmail.com>
2012-04-08 11:06:03 +0800

    make table to save time,pass

--- CalendarReform.c
+++ CalendarReform.c
@@ -1,43 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-long long month(long long a)
-{
-	long long i;
-
-	long long months=a;
-	long long days=1;
-	for(i=2; i*i<=a; i++)
-	{
-		long long temp;
-		temp=i*i;
-		if(a%temp==0)
-		{
-			months=a/temp;
-		}
-	}
-
-	return months;
-}
-
-
-	
+#define M 10000000
+long long x[M+1];
 int main()
 {
 	long long a,n;
+	long long i,t,j,k,sum;
 
-	scanf("%I64d%I64d",&a,&n);
-	long long p=0;
-	long long i=0;
-	for(i=0; i<n; i++)
+	for(i=1; (t=i*i)<=M; i++)
 	{
-		if(i==0)
-		    a=a+i;
-		else
-			a=a+1;
-		p=p+month(a);
+		for(j=1, k=t; k<=M; j++,k=k+t)
+			x[k]=j;
 	}
 
-	printf("%I64d\n",p);
+	scanf("%I64d%I64d",&a,&n);
+	for(sum=0,i=0; i<n; i++)
+		sum=sum+x[a+i];
+	printf("%I64d\n",sum);
+
 	return 0;
 }
